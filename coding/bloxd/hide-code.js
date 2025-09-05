2eeb63d33969f856e79eaac3f83205c6dcfd58d2
Power Trip <96303911+InsanePowerTrip@users.noreply.github.com>
2025-09-05 10:57:52 +0400

    adding toasts and making the js more accurate. idk if this will work tbh

--- coding/bloxd/hide-code.js
+++ coding/bloxd/hide-code.js
@@ -19,6 +19,12 @@ onPlayerStartChargingItem onPlayerFinishChargingItem doPeriodicSave`;
 
 const allCallbacks = allValidCallbackString.split(/\s+/);
 
+const badToast = document.getElementById('badToast');
+const badToastBs = bootstrap.Toast.getOrCreateInstance(badToast);
+
+const goodToast = document.getElementById('goodToast');
+const goodToastBs = bootstrap.Toast.getOrCreateInstance(goodToastBs);
+
 function onSubmitHidecode() {
     const enteredCallbacks = document.getElementById("callbacks").value.split(/\s+/);
     const worldCode = document.getElementById("ogcode").value;
@@ -27,12 +33,15 @@ function onSubmitHidecode() {
     let resultWorldCode = "let nd=false,ltndt=false;methods={};lc=(n,p=null)=>{try{const m=methods[n];if(!m){if(p)api.sendMessage(p,`Error: Callback ${n} doesnt work! Please wait for someone to fix this!`);nd=true;return false}return m}catch(e){if(p)api.sendMessage(p,`Error: ${e}! Please wait for the owner to fix this!`);nd=true;return false}};tick=()=>{if(nd){try{typeof methods.tick=='function'&&(methods.tick(),nd=false,ltndt=true)}catch(e){nd=true,ltndt=false}}!nd&&ltndt&&(ltndt=false,api.broadcastMessage('Someone has fixed!'),api.getPlayerIds().forEach(id=>onPlayerJoin(id)))};\n";
     for (let i=0; i!=enteredCallbacks.length; i++) {
         if (!(allCallbacks.includes(enteredCallbacks[i]))) {
-            document.getElementById("worldcode").value = `Error! << ${enteredCallbacks[i]} >> isn't a valid callback!`;
-            document.getElementById("codeblock").value = "Error. Read << World Code >> message above!";
+            document.getElementById("worldcode").value = '';
+            document.getElementById("codeblock").value = '';
+            badToastBs.show()
             return;
         };
         resultBlockCode += `methods.${enteredCallbacks[i]} = ${enteredCallbacks[i]};`;
+        if (enteredCallbacks[i] === "tick") continue;
         resultWorldCode += `${enteredCallbacks[i]}=(...a)=>{const f=lc("${enteredCallbacks[i]}",a[0]);f&&f(...a)};\n`
+        
     }
 
     if (!(enteredCallbacks.includes("tick"))) {
@@ -41,4 +50,5 @@ function onSubmitHidecode() {
 
     document.getElementById("worldcode").value = resultWorldCode;
     document.getElementById("codeblock").value = resultBlockCode;
+    goodToastBs.show()
 }
